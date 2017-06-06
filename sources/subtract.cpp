b75ae9f7ac74aca85703cadc0e50a33a81e734ab
AnastasiaPetlyakova <petlyakova.a@yandex.ru>
2017-06-06 22:18:43 +0300

    Update subtract.cpp

--- sources/subtract.cpp
+++ sources/subtract.cpp
@@ -1,13 +1,32 @@
-#include "yadisk/ops/subtract.hpp"
+#include "include/yadisk/ops/subtract.hpp"
 
+/**
+Выполняет сравнение хэш-функций по алгоритму SHA-256
+\param[in] json одного дерева
+\param[in] json другого дерева
+\return true, если хэши совпадают, false в противном случае
+**/
 bool SHA256(const json& left, const json& right) {
 	return left["sha256"] == right["sha256"];
 }
 
+/**
+Выполняет сравнение хэш-функций по алгоритму MD5
+\param[in] json одного дерева
+\param[in] json другого дерева
+\return true, если хэши совпадают, false в противном случае
+**/
 bool MD5(const json& left, const json& right) {
 	return left["md5"] == right["md5"];
 }
 
+/**
+Выполняет вычитание двух деревьев
+\param[in] первое дерево
+\param[in] второе дерево
+\param[in] предикат (true, если ресурсы совпадают; false в противном случае)
+\return поддерево, являющееся результатом вычитания второго дерева из первого
+**/
 auto Operations::subtract(Tree tree1, Tree tree2, Predicate pred) -> Tree&
 {
 	json flat_tree1 = tree1.flat_list();
