492fdfb5928ecd5ea7404053ed5e90b7e6f6077c
AnastasiaPetlyakova <petlyakova.a@yandex.ru>
2017-06-06 22:23:27 +0300

    Update subtract.hpp

--- include/yadisk/ops/subtract.hpp
+++ include/yadisk/ops/subtract.hpp
@@ -8,13 +8,32 @@
 using yadisk::ops::Resource;
 using yadisk::ops::Predicate;
 using yadisk::ops::Tree;
-
+/**
+Выполняет сравнение хэш-функций по алгоритму SHA-256
+\param[in] json одного дерева
+\param[in] json другого дерева
+\return true, если хэши совпадают, false в противном случае
+**/
 bool SHA256(const json& left, const json& right);
-
+/**
+Выполняет сравнение хэш-функций по алгоритму MD5
+\param[in] json одного дерева
+\param[in] json другого дерева
+\return true, если хэши совпадают, false в противном случае
+**/
 bool MD5(const json& left, const json& right);
 
 namespace Operations
 {
+	
+/**
+Выполняет вычитание двух деревьев
+\param[in] первое дерево
+\param[in] второе дерево
+\param[in] предикат (true, если ресурсы совпадают; false в противном случае)
+\return поддерево, являющееся результатом вычитания второго дерева из первого
+\include examples/example.cpp
+**/
 	auto subtract(Tree tree1, Tree tree2, Predicate pred = SHA256) -> Tree&;
 }
 
