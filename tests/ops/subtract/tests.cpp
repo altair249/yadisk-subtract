52cf8bf7fa66da147d7503132dd1dbb1a90af22a
AlexOdlin <rockguitarist94alex@gmail.com>
2017-06-08 00:50:43 +0300

    Update tests.cpp

--- tests/ops/subtract/tests.cpp
+++ tests/ops/subtract/tests.cpp
@@ -1,5 +1,6 @@
 #include <catch.hpp>
 #include <yadisk/ops/subtract.hpp>
+using ydsubtract = yadisk::ops::subtract;
 
 
 TEST_CASE("subtract empty tree1 and full tree2", "[ops][subtract][tree]") {
@@ -8,8 +9,8 @@ TEST_CASE("subtract empty tree1 and full tree2", "[ops][subtract][tree]") {
   yadisk::ops::Tree tree1;
   yadisk::ops::Tree tree2;
   //When
-  
+  auto res = ydsubtract::Operations::subtract(tree1, tree2)
   //Then
-  REQUIRE()
+  REQUIRE(res.epmpty())
   
   }
