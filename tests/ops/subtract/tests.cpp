c4bdf2b2908b83eea027c55998e219610fab05d7
AnastasiaPetlyakova <petlyakova.a@yandex.ru>
2017-06-08 01:28:52 +0300

    Update tests.cpp

--- tests/ops/subtract/tests.cpp
+++ tests/ops/subtract/tests.cpp
@@ -18,3 +18,51 @@ TEST_CASE("subtract empty tree1 and full tree2", "[ops][subtract][tree]") {
   REQUIRE(res == tree1)
   
   }
+
+TEST_CASE("subtract full tree1 and empty tree2", "[ops][subtract][tree]") {
+
+  //Given
+  yadisk::ops::Tree tree1;
+  yadisk::ops::Tree tree2;
+  json flat1 = tree1.flat_list();
+  json flat2 = tree2.flat_list();
+    //When
+  flat1 != 0;
+  flat2 == 0;
+  auto res = ydsubtract::Operations::subtract(tree1, tree2)
+  //Then
+  REQUIRE(res == tree1)
+  
+  }
+
+TEST_CASE("subtract empty tree1 and empty tree2", "[ops][subtract][tree]") {
+
+  //Given
+  yadisk::ops::Tree tree1;
+  yadisk::ops::Tree tree2;
+  json flat1 = tree1.flat_list();
+  json flat2 = tree2.flat_list();
+    //When
+  flat1 == 0;
+  flat2 == 0;
+  auto res = ydsubtract::Operations::subtract(tree1, tree2)
+  //Then
+  REQUIRE(res == tree1)
+  
+  }
+
+TEST_CASE("subtract full tree1 and full tree2", "[ops][subtract][tree]") {
+
+  //Given
+  yadisk::ops::Tree tree1;
+  yadisk::ops::Tree tree2;
+  json flat1 = tree1.flat_list();
+  json flat2 = tree2.flat_list();
+    //When
+  flat1 != 0;
+  flat2 != 0;
+  auto res = ydsubtract::Operations::subtract(tree1, tree2)
+  //Then
+  REQUIRE(not res.empty())
+  
+  }
