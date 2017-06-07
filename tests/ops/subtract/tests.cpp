074dbbbafd3b52291738e341aae9752a0ccf156e
AnastasiaPetlyakova <petlyakova.a@yandex.ru>
2017-06-08 01:08:01 +0300

    Update tests.cpp

--- tests/ops/subtract/tests.cpp
+++ tests/ops/subtract/tests.cpp
@@ -8,10 +8,10 @@ TEST_CASE("subtract empty tree1 and full tree2", "[ops][subtract][tree]") {
   //Given
   yadisk::ops::Tree tree1;
   yadisk::ops::Tree tree2;
-  //When
   json flat1 = tree1.flat_list();
-  flat1 == 0;
   json flat2 = tree2.flat_list();
+    //When
+  flat1 == 0;
   flat2 != 0;
   auto res = ydsubtract::Operations::subtract(tree1, tree2)
   //Then
