602123b858f660cfaa2362456e27b9a31e06baf7
AlexOdlin <rockguitarist94alex@gmail.com>
2017-06-08 00:31:20 +0300

    Update tests.cpp

--- tests/ops/subtract/tests.cpp
+++ tests/ops/subtract/tests.cpp
@@ -1,2 +1,15 @@
 #include <catch.hpp>
 #include <yadisk/ops/subtract.hpp>
+
+
+TEST_CASE("subtract empty tree1 and full tree2", "[ops][subtract][tree]") {
+
+  //Given
+  yadisk::ops::Tree tree1;
+  yadisk::ops::Tree tree2;
+  //When
+  
+  //Then
+  REQUIRE()
+  
+  }
